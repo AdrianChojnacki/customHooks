203815a31c3724066819d9e83858f07e126b302b
Adrian Chojnacki <achojnacki@pgs-soft.com>
2022-09-27 15:23:35 +0200

    Add useDebounce

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ToggleComponent from "./ToggleComponent";
 import TimeoutComponent from "./TimeoutComponent";
+import DebounceComponent from "./DebounceComponent";
 
 export default function App() {
   return (
     <>
       <ToggleComponent />
       <TimeoutComponent />
+      <DebounceComponent />
     </>
   );
 }
