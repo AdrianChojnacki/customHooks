d2bf5bdac4f3968039afc5aa4dd1c3d84ecd1c99
Adrian Chojnacki <achojnacki@pgs-soft.com>
2022-09-27 17:28:57 +0200

    add useArray

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import ToggleComponent from "./ToggleComponent";
 import TimeoutComponent from "./TimeoutComponent";
 import DebounceComponent from "./DebounceComponent";
 import UpdateEffectComponent from "./UpdateEffectComponent";
+import ArrayComponent from "./ArrayComponent";
 
 export default function App() {
   return (
     <>
-      {/* <ToggleComponent /> */}
-      {/* <TimeoutComponent /> */}
-      {/* <DebounceComponent /> */}
+      <ToggleComponent />
+      <TimeoutComponent />
+      <DebounceComponent />
       <UpdateEffectComponent />
+      <ArrayComponent />
     </>
   );
 }
