f589cb38ffe01077eb134ce3e55d9109e2e5cf27
Adrian Chojnacki <achojnacki@pgs-soft.com>
2022-09-27 14:51:37 +0200

    Add useTimeout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import ToggleComponent from "./ToggleComponent";
+import TimeoutComponent from "./TimeoutComponent";
 
 export default function App() {
-  return <ToggleComponent />;
+  return (
+    <>
+      {/* <ToggleComponent /> */}
+      <TimeoutComponent />
+    </>
+  );
 }
